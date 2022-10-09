45239ceb97f7d8be7aaae9cd9f0f7dfba19802d9
Shawaal-Nadeem <hafizshawalnadeem@gmail.com>
2022-10-09 10:51:15 -0700

    Update Responsive

--- src/components/iPhoneGen/iphonese.js
+++ src/components/iPhoneGen/iphonese.js
@@ -1,14 +1,18 @@
-import React from 'react';
-
+import React from "react";
 
 function IphoneSe() {
   return (
     <div>
-    <h1 className='headingAllIphone'>I Phone SE </h1>
-    <h3 className='ReleaseDate'>Release Date: March 31, 2016</h3>
-    <img className='se' src='/images/1n.png'></img>
- 
-    <p className='detailPhone'>Don’t think I’ve forgotten the iPhone SE. It had all the awesome internal specs of the iPhone 6s in a small package and without 3D Touch. But overall, the iPhone SE was introduced as a more affordable option that people really loved.</p>
+      <h1 className="headingAllIphone">I Phone SE </h1>
+      <h3 className="ReleaseDate">Release Date: March 31, 2016</h3>
+      <img className="se" src="/images/1n.png"></img>
+
+      <p className="detailPhone">
+        Don’t think I’ve forgotten the iPhone SE. It had all the awesome
+        internal specs of the iPhone 6s in a small package and without 3D Touch.
+        But overall, the iPhone SE was introduced as a more affordable option
+        that people really loved.
+      </p>
     </div>
   );
 }
